14c80ff0b9adecdf25c7b5f3f543cdb5f3d4261c
William Prieto <wprietovelandia@gmail.com>
2021-10-09 15:47:37 -0500

    Nuevo proyecto de Spring-Boot con maven, estructura base

---
+++ fuentes/microservicios/src/main/java/com/misiontic/microservicios/controllers/ResearcherController.java
@@ -0,0 +1,25 @@
+package com.misiontic.microservicios.controllers;
+
+import com.misiontic.microservicios.dtos.ResearcherDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class ResearcherController {
+
+    @GetMapping("/researcher")
+    public ResearcherDto getResearcher() {
+        LocalDateTime entryDate = LocalDateTime.of(2021, 11, 25, 0, 0, 0, 0);
+        return new ResearcherDto("61461ba90f9b95fb06d365b0",
+                101,
+                "Estudiante",
+                "Hector",
+                "Osorio",
+                3106008455L,
+                entryDate,
+                "Mercadeo",
+                "614611590f9b95fb06d365a4");
+    }
+}
---
+++ fuentes/microservicios/src/test/java/com/misiontic/microservicios/MicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.microservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
