96304a7266807727cf1053b64048640afa73c1b4
Fernando Díaz <feeldiac@gmail.com>
2021-10-13 18:47:46 -0500

    Arreglado typo en App.js

---
+++ fuentes/microservicios/src/main/java/com/misiontic/microservicios/controllers/ResearcherController.java
@@ -0,0 +1,79 @@
+package com.misiontic.microservicios.controllers;
+
+import com.misiontic.microservicios.dtos.ResearcherDto;
+import com.misiontic.microservicios.models.Researcher;
+import com.misiontic.microservicios.repositories.ProjectRepository;
+import com.misiontic.microservicios.repositories.ResearcherRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/participantes")
+public class ResearcherController {
+
+    private final ResearcherRepository researcherRepository;
+    private final ProjectRepository projectRepository;
+
+    public ResearcherController(ResearcherRepository researcherRepository, ProjectRepository projectRepository) {
+        this.researcherRepository = researcherRepository;
+        this.projectRepository = projectRepository;
+
+        Researcher r1 = new Researcher("E001",
+                "Estudiante",
+                "Hector",
+                "Osorio",
+                310600845,
+                LocalDateTime.of(2021, 11, 25, 0, 0, 0, 0),
+                "Mercadeo",
+                "P001"
+                );
+        Researcher r2 = new Researcher("E002",
+                "Estudiante",
+                "Juan",
+                "Mejia",
+                311123455,
+                LocalDateTime.of(2021, 12, 10, 0, 0, 0, 0),
+                "Ingenieria Ambiental",
+                "P001"
+        );
+
+        this.researcherRepository.save(r1);
+        this.researcherRepository.save(r2);
+    }
+
+    // Mostrar todos los participantes y filtrar por ProjectId
+    @GetMapping
+    public List<Researcher> getAllResearcher(@RequestParam(required = false) String projectId) {
+        List<Researcher> result = researcherRepository.findAll();
+        if (projectId == null) {
+            return result;
+        }
+        else {
+            return result.stream()
+                    .filter(r -> r.getProjectId().equals(projectId))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    // Mostrar un participante en especifico
+    @GetMapping("/{researcherId}")
+    public Researcher getOneResearcher(@PathVariable String researcherId) {
+        return researcherRepository.findById(researcherId).orElse(null);
+    }
+
+    // Actualizar los datos de un participante en especifico
+    @PutMapping("/{researcherId}/actualizar")
+    public void updateResearcher(@PathVariable String researcherId,
+                                 @RequestBody Researcher actualResearcher) {
+        researcherRepository.save(actualResearcher);
+    }
+
+    // Nuevo ingreso de un participante
+    @PostMapping("/ingresar")
+    public void updateResearcher(@RequestBody Researcher newResearcher) {
+        researcherRepository.save(newResearcher);
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
 import './App.css';
 import react from 'react';
-import Proyectos from './Proyectos.jsx'
-import Participantes from './Participantes.js'
+import Proyectos from './Proyectos.jsx';
+import Participantes from './participantes.js';
 import axios from 'axios';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
 
 function App() {
-
   const projecto = {
-    _id: "p01",
-    title: "nombre del proyecto p01",
+    _id: 'p01',
+    title: 'nombre del proyecto p01',
     description: 'desc proj p01',
-    status: 'iniciado'
-  }
+    status: 'iniciado',
+  };
 
-  const listaProyectos= [
-    "Proyecto1",
-    "prpyecto2"
-  ]
+  const listaProyectos = ['Proyecto1', 'prpyecto2'];
 
   const persona = {
     _id: 'r01',
     role: 'estudian',
     cell: '3256987410',
-    carrer: 'biomedico'
-  }
+    carrer: 'biomedico',
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className ="ProjectoRAMD">RAMD</h1>
-        <nav className = "navegacion">
-          <button className = "botonNav" >Lista de Proyectos</button>
-          <button className = "botonNav">Lista de personas</button>
+        <h1 className="ProjectoRAMD">RAMD</h1>
+        <nav className="navegacion">
+          <button className="botonNav">Lista de Proyectos</button>
+          <button className="botonNav">Lista de personas</button>
         </nav>
-        <p>Cada uno de los links anteriores lleva a un componente que llama la informacion del backend</p>
+        <p>
+          Cada uno de los links anteriores lleva a un componente que llama la
+          informacion del backend
+        </p>
       </header>
-        <section className="Consulta proyectos">
-          <div className = "Consultas">
-            <div className = "ConsultaParticipantes">
-              <Participantes/>
-            </div>
-            <div className = "ConsultaProyectos">
-              <Proyectos/>
-            </div>
+      <section className="Consulta proyectos">
+        <div className="Consultas">
+          <div className="ConsultaParticipantes">
+            <Participantes />
           </div>
-        </section>
+          <div className="ConsultaProyectos">
+            <Proyectos />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
